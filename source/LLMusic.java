102ccdcf89b89f64bd8d9b0367e5122177279d24
Sebastian Foubert <sebastian2696@gmail.com>
2018-04-20 03:17:51 -0400

    Cleanup 2

--- source/LLMusic.java
+++ source/LLMusic.java
@@ -1,43 +1,43 @@
 import java.util.*;
 
 class Song{
-            String Artist;
-            String Album;
-            String Song_Title;
-            private String File_Path;
-            private String Image_Path;
+    String Artist;
+    String Album;
+    String Song_Title;
+    private String File_Path;
+    private String Image_Path;
 
-            public Song(String Artist, String Album, String Song_Title, String File_Path, String Image_Path){
-                this.Artist = Artist;
-                this.Album = Album;
-                this.Song_Title = Song_Title;
-                this.File_Path = File_Path;
-                this.Image_Path = Image_Path;
-            }
+    public Song(String Artist, String Album, String Song_Title, String File_Path, String Image_Path){
+        this.Artist = Artist;
+        this.Album = Album;
+        this.Song_Title = Song_Title;
+        this.File_Path = File_Path;
+        this.Image_Path = Image_Path;
+    }
 
-            public String getFile_Path(){
-                return File_Path;
-            }
+    public String getFile_Path(){
+        return File_Path;
+    }
 
-            public String getImage_Path(){
-                return Image_Path;
-            }
+    public String getImage_Path(){
+        return Image_Path;
+    }
 
 }
 
 public class LLMusic{
-        LinkedList<Song> songLL;
+    LinkedList<Song> songLL;
 
-        public LLMusic(){
-            songLL = new LinkedList<Song>();
-        }
+    public LLMusic(){
+        songLL = new LinkedList<Song>();
+    }
 
-        public boolean songAdd(Song song){
-            return songLL.add(song);
-        }
+    public boolean songAdd(Song song){
+        return songLL.add(song);
+    }
 
-        public void ll_Content(){
-            for (Song content:songLL)
-                System.out.println(content.Song_Title);
-        }
+    public void ll_Content(){
+        for (Song content:songLL)
+            System.out.println(content.Song_Title);
+    }
 }
